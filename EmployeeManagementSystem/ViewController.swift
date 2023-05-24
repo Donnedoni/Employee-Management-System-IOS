5d760a0ad67a67921007b31af0ac469c0ae6238a
Donnedoni Fillies <donnedonifillies@gmail.com>
2023-05-24 16:13:15 +0200

    first commit

---
+++ EmployeeManagementSystem/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmployeeManagementSystem
+//
+//  Created by DA MAC M1 150 on 2023/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
