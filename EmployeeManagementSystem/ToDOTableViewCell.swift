226a9bb01d82ca55f134a6df98d1c127371ffce5
Mukosi Budeli <mukosibudeli@gmail.com>
2023-05-25 09:09:06 +0200

    Added form storyboad

---
+++ EmployeeManagementSystem/ToDOTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ToDOTableViewCell.swift
+//  EmployeeManagementSystem
+//
+//  Created by DA MAC M1 115 on 2023/05/25.
+//
+
+import UIKit
+
+class ToDOTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ EmployeeManagementSystem/ToDOViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ToDOViewController.swift
+//  EmployeeManagementSystem
+//
+//  Created by DA MAC M1 115 on 2023/05/25.
+//
+
+import UIKit
+
+class ToDOViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
